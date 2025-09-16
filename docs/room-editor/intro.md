508920bed163842f40e3ea14e2271df10d1d69f2
Jurica Adamek <jura1994jk@hotmail.com>
2025-09-16 22:54:50 +0200

    w

--- docs/room-editor/intro.md
+++ docs/room-editor/intro.md
@@ -1,1 +1,14 @@
-# ROOM EDITOR
+# Room Editor
+
+Create immersive rooms with Room Editor 2.0
+
+# New Features
+
+- Realtime Lightning and Shadows
+- Materials
+- Floor/Wall Building system
+- Elevation using angled floors, stairs and ladders
+- Updated LUA support
+- Even more logic props and behaviours
+- Modern slick UI/UX
+- Assets window for easier asset management
--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,4 @@
 const sidebars = {
-
   gameSidebar: [
     { type: 'doc', id: 'intro/intro', },
     { type: 'doc', id: 'patch-history', },
@@ -8,9 +7,10 @@ const sidebars = {
   ],
 
   roomEditorSidebar: [
-    { type: 'doc', id: 'room-editor/intro', }
+    { type: 'doc', id: 'room-editor/intro', },
+    { type: 'doc', id: 'room-editor/navigation', },
+    { type: 'doc', id: 'room-editor/faq/faq', },
   ],
-
 };
 
 export default sidebars;
